1cd8b925cc86e6bf34fd38ab12415b455e4df775
Biki-98 <parthaskill98@gmail.com>
2026-01-27 08:59:18 +0530

    Add training pipeline

---
+++ src/student_score_predictor/pipeline/train_pipeline.py
@@ -0,0 +1,27 @@
+import sys
+from ..components.data_ingestion import DataIngestionConfig, DataIngestion
+from ..components.data_transformation import DataTransformationConfig, DataTransformation
+from src.exception import CustomException
+from src.logger import logging
+
+def run_pipeline():
+    try:
+        logging.info("---Staring Pipeline---")
+        # start data ingestion
+        data_ingestion = DataIngestion()
+        train_data, test_data = data_ingestion.start_data_ingestion()
+    
+        # start preprocessing/transformation
+        data_transformation = DataTransformation()
+        # train_transformed, test_transformed, prep_obj_path  = (data_transformation.
+                                                # initiate_data_transformation(train_data,test_data))
+        data_transformation.initiate_data_transformation(train_data,test_data)
+        logging.info("---Pipeline executed successfully---")
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+if __name__=="__main__":
+    run_pipeline()
+    
