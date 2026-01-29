15d10e00e609c719ffc26fad2d1a7bd90f1b780b
Biki-98 <parthaskill98@gmail.com>
2026-01-29 23:18:11 +0530

    Add model traning to the pipeline

--- src/student_score_predictor/pipeline/train_pipeline.py
+++ src/student_score_predictor/pipeline/train_pipeline.py
@@ -1,6 +1,7 @@
 import sys
 from ..components.data_ingestion import DataIngestionConfig, DataIngestion
 from ..components.data_transformation import DataTransformationConfig, DataTransformation
+from ..components.model_trainer import ModelTrainerConfig, ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 
@@ -15,7 +16,11 @@ def run_pipeline():
         data_transformation = DataTransformation()
         # train_transformed, test_transformed, prep_obj_path  = (data_transformation.
                                                 # initiate_data_transformation(train_data,test_data))
-        data_transformation.initiate_data_transformation(train_data,test_data)
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,
+                                                                                  test_data)
+        
+        model_trainer = ModelTrainer()
+        model_trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr)
         logging.info("---Pipeline executed successfully---")
     
     except Exception as e:
