f53100164cc7e8cc552acb19f818a0ce03601269
Biki-98 <parthaskill98@gmail.com>
2026-02-02 20:39:36 +0530

    Modify model names and logic change

--- src/student_score_predictor/components/model_trainer.py
+++ src/student_score_predictor/components/model_trainer.py
@@ -15,8 +15,10 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from student_score_predictor.utils.file_utils import save_object, save_json
-from student_score_predictor.utils.evaluate_model import evaluate_model
+from student_score_predictor.utils.file_utils import save_object, save_json, read_yaml
+from student_score_predictor.utils.evaluate_model import evaluate_model, tune_model
+
+params = read_yaml("params.yaml")
 
 @dataclass
 class ModelTrainerConfig:
@@ -39,44 +41,44 @@ class ModelTrainer:
             logging.info("Spliting of training and test input data completed successfully.")
             
             models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "random_forest": RandomForestRegressor(),
+                "decision_tree": DecisionTreeRegressor(),
+                "gradient_boosting": GradientBoostingRegressor(),
+                "linear_regression": LinearRegression(),
+                "xGBRegressor": XGBRegressor(),
+                # "catBoosting_regressor": CatBoostRegressor(verbose=False),
+                # "adaBoost_regressor": AdaBoostRegressor(),
             }
             
-            logging.info("Starting Training + Prediction + Evaluation of the models-->")
+            logging.info("Starting Training + Prediction + Evaluation of the models.")
             
             model_report: dict = evaluate_model(X_train=X_train, y_train=y_train,
                                                 X_test=X_test, y_test=y_test,
-                                                models=models)
+                                                models=models,
+                                                params=params,
+                                                hyperparameter_tuning=True)
+            
             logging.info("Training + Prediction + Evaluation of the models completed successfully.")
-
-            # To get the best model score from the model_report
-            best_model_name, best_model_score = max(model_report.items(), key=lambda x: x[1])
+            
+            # To get the best model name & score from the model_report
+            best_model_name, best_model_info = max(model_report.items(),key=lambda x: x[1]["best_pred_score"])
+            best_score = best_model_info["best_pred_score"]
 
             logging.info("Best model name and score is obtained successfully.")
 
-            if best_model_score < 0.6:
+            if best_score < 0.6:
                 raise CustomException("Best model score is less than 0.6, no best model found")
             
-            logging.info(f"Best model {best_model_name} , score: {best_model_score}")
+            logging.info(f"Best model {best_model_name} , score: {best_score}")
 
-            save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj = best_model_name
-            )
+            save_object(file_path = self.model_trainer_config.trained_model_file_path,
+                        obj = best_model_name
+                        )
             
             # Saving the model report as a json file in artifacts
             save_json(model_report, "artifacts/model_report.json")
             
             logging.info("Model report is saved inside artifacts")
-            
-            return f"Best model {best_model_name} , score: {best_model_score}"
-
 
         except Exception as e:
             raise CustomException(e, sys)
